3b24957b28b6bc86cd07d43eec04d463a277e8f3
Анастасия Мартынкевич <semenova.a.sergeevna@gmail.com>
2022-04-26 12:29:12 +0300

    Create class WallService

--- src/main/kotlin/ru/netology/data/Post.kt
+++ src/main/kotlin/ru/netology/data/Post.kt
@@ -1,4 +1,4 @@
-package ru.netology
+package ru.netology.data
 
 import java.util.*
 
---
+++ src/main/kotlin/ru/netology/service/WallService.kt
@@ -0,0 +1,30 @@
+package ru.netology.service
+
+import ru.netology.data.Post
+
+object WallService {
+
+    private var posts = emptyArray<Post>()
+
+    private var property = 0
+
+    fun add(post: Post): Post {
+        val newPost = post.copy(id = property + 1)
+        posts += newPost
+        property++
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for (item in posts){
+            if (item.id == post.id) {
+                val number = posts.indexOf(item)
+                posts[number]  = post.copy(id = item.id, ownerId = item.ownerId, date = item.date)
+               return true
+            } else
+                continue
+        }
+        return false
+    }
+
+}
